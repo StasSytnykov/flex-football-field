72995653271413ed591ef20b091f16256887d293
StasSytnykov <stas82kak05@gmail.com>
2023-04-04 11:55:05 +0300

    deleted base url

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  output: "export",
-  basePath: "/flex-football-field",
   env: {
     GOOGLE_API_KEY: process.env.GOOGLE_API_KEY,
   },
