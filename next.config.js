6f099ca520c290d45f3fce8ea5bda590926fbb43
StasSytnykov <stas82kak05@gmail.com>
2023-04-04 11:53:11 +0300

    added base url

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  output: "export",
+  basePath: "/flex-football-field",
   env: {
     GOOGLE_API_KEY: process.env.GOOGLE_API_KEY,
   },
